b345f12b16bfef9383136c1307aadeade53e1f08
Seya <31957516+sglkc@users.noreply.github.com>
2024-11-24 21:28:40 +0700

    build(pnpm): add font & icon

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import './assets/main.css'
+import '@unocss/reset/tailwind.css'
+import '@fontsource-variable/sora'
 import 'virtual:uno.css'
 
 import { createApp } from 'vue'
--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,39 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, presetUno, presetIcons } from 'unocss'
 
 export default defineConfig({
-
+  presets: [
+    presetUno(),
+    presetIcons({
+      collections: {
+        mci: () => import('@iconify-json/mingcute/icons.json').then(i => i.default)
+      },
+      warn: true,
+    })
+  ],
+  shortcuts: {
+    'bg-base': 'bg-white dark:bg-black',
+    'text-base': 'text-gray-900 dark:text-gray-300',
+    'b-base': 'b-black dark:border-white',
+    'colors-primary': 'bg-black text-white dark:bg-white dark:text-black',
+    'colors-secondary': 'bg-white text-black dark:bg-black dark:text-white',
+  },
+  theme: {
+    fontFamily: {
+      body: '"Sora Variable", sans-serif',
+    },
+    boxShadow: {
+      base: '4px 4px',
+      sm: '8px 8px',
+      md: '16px 16px'
+    }
+  },
+  variants: [
+    (matcher) => {
+      if (!matcher.startsWith('on:')) return matcher
+      return {
+        matcher: matcher.slice(3),
+        selector: s => `${s}:hover, ${s}:focus`,
+      }
+    },
+  ],
 })
