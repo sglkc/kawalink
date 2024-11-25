941b5ebe5824ef6a279eea3d28a0e3796a364832
Seya <31957516+sglkc@users.noreply.github.com>
2024-11-25 19:04:25 +0700

    feat(FileUpload): add drag-n-drop, file size

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,26 +1,64 @@
 <script setup lang="ts">
-const model = defineModel<File | null>()
+import { ref } from 'vue'
 
-function onChange(e: Event) {
-  const { files } = (e.currentTarget as HTMLInputElement)
-  const file = files?.item(0)
+const model = defineModel<File | null>()
+const isOnTop = ref(false)
+const sizeFormatter = new Intl.NumberFormat([], {
+  style: 'unit',
+  unit: 'byte',
+  notation: 'compact',
+  unitDisplay: 'narrow'
+})
+
+function onDragOver(e: DragEvent) {
+  e.preventDefault()
+  isOnTop.value = true
+}
+
+function onDragLeave() {
+  isOnTop.value = false
+}
+
+function onFileChange(e: Event | DragEvent) {
+  e.preventDefault()
+
+  let file
+
+  if ('dataTransfer' in e && e.dataTransfer) {
+    file = e.dataTransfer.files?.item(0)
+  } else {
+    file = (e.currentTarget as HTMLInputElement).files?.item(0)
+  }
+
+  isOnTop.value = false
+
+  if (!file) return
 
   model.value = file
 }
 </script>
 
 <template>
-  <label :class="[
-    'px-4 py-8 b-2 b-black b-dashed shadow-base rounded grid gap-2 justify-center',
-    'cursor-pointer'
-  ]">
-    <div>{{ model ? model.name : 'Click or drag-and-drop a file to send' }}</div>
+  <label
+    class="px-4 py-8 b-2 b-black shadow-base rounded grid gap-2 justify-center text-center cursor-pointer"
+    :class="{ 'bg-green-100': isOnTop, 'b-dashed': !model }"
+    @dragover="onDragOver"
+    @dragleave="onDragLeave"
+    @drop="onFileChange"
+  >
+    <p class="touch-none select-none truncate">
+      {{ model ? model.name : 'Click or drag-and-drop a file to send' }}
+    </p>
     <div
-      class="text-4xl m-auto"
-      :class="{ 'i-mci:upload-line': !model, 'i-mci:file-line': model }"
+      class="touch-none select-none text-4xl m-auto"
+      :class="{
+        'i-mci:upload-line': !model,
+        'i-mci:file-line': model,
+      }"
     />
+    <p class="text-sm text-gray-500">{{ model?.size && sizeFormatter.format(model.size) }}</p>
     <input
-      @change="onChange"
+      @change="onFileChange"
       class="hidden"
       type="file"
     />
